1c53622c7bb68ff1184291b1601e3a56704d7ff2
jesus Tonconi <jesusbrere@gmail.com>
2025-08-03 03:05:18 -0400

    recuperado

--- src/certificado.sol
+++ src/certificado.sol
@@ -1,70 +1,128 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract contrato{
+contract contrato is ReentrancyGuard{
     address owner;
-    mapping(address => bool) public esValidador;
+    mapping(address => bool) public esIntermediario;
     uint256 public contadorOperaciones = 0;
+    uint256 porcentajeComision;
+    uint256 ganancia=0;
 
-    enum Rol { Importador, Exportador }
-    enum Estado { Pendiente, Validado, Imcompleto }
+    enum Estado { Pendiente, exitoso, enDisputa,resuelto}
+    enum TipoDeExportador{intermediario,productor}
 
-    struct Usuario {
-        string nombre;
-        Rol rol;
+    struct Exportador {
+        string nombreComercial;
+        string codigoDeComercio;
+        string nombreRepresntante;
+        string producto;
+        TipoDeExportador tipoE;
         uint256 reputacion;
         bool registrado;
+    }
+    struct Importador{
+        string nombreEmpresa;
+        string nombreRepresentante;
+        uint256 reputacion;
+        bool registrado;
+        
     }
     struct Operacion{
         address importador;
         address exportador;
         uint256 monto;
-        Estado estado; 
+        Estado estado;
     }
     
-    mapping(address => Usuario) usuarios;
+    mapping(address => Exportador) exportadores;
+    mapping(address => Importador) importadores;
     mapping(uint256 => Operacion) public operaciones;
     mapping(address => uint[]) opUsuarios;
+    address[] intermediarios;
 
-    event Registro(string indexed nombre,Rol rol);
+    event Registro(string indexed nombre,string tipo);
     event nuevaOperacion(address importador,address exportador,uint256 monto);
+    event OperacionExitosa(uint256 id);
+    event DisputaResuelta(uint256 indexed id, address ganador, address intermediario);
 
+
+    error noSePundoRealizarTransaccion();
     constructor(){
         owner=payable(msg.sender);
+        porcentajeComision = 5;
     }
-    function registrarValidador(address validador)public{
+    function registrarIntermediario(address validador)public{
         require(msg.sender == owner);
-        esValidador[msg.sender]= true;
+        esIntermediario[validador] = true;
     }
 
-    function registro(string memory nombre,Rol rol) external {
-        require(!usuarios[msg.sender].registrado,"adress ya esta registrado registrado");
-        usuarios[msg.sender]=Usuario(nombre,rol,0,true);
-        emit Registro(nombre,rol);
+    function registroImportador(
+        string memory nombreEmpresa,string memory Representante) external {
+        require(!importadores[msg.sender].registrado, "Ya registrado");
+        importadores[msg.sender] = Importador(nombreEmpresa,Representante,0,true);
+        
+        emit Registro(nombreEmpresa,"Importador");
+    }
+    function registroExportador(
+        string memory nombreEmpresa,string memory codigoDeComercio,string memory Representante,string memory producto
+        ,TipoDeExportador tipoE) external {
+        require(!exportadores[msg.sender].registrado, "Ya registrado");
+        exportadores[msg.sender] = Exportador(nombreEmpresa,codigoDeComercio,Representante,producto,tipoE,0,true);
+        
+        emit Registro(nombreEmpresa,"Exportador");
     }
 
     function hacerPedido(address exportador) payable public{
-        require(usuarios[msg.sender].registrado,"adress no registrado");
-        require(usuarios[msg.sender].rol==Rol.Importador,"adress registrado como exportador");
+        require(importadores[msg.sender].registrado,"adress no registrado como importador");
 
         Operacion memory op=Operacion(msg.sender,exportador,msg.value,Estado.Pendiente);
         contadorOperaciones +=1;
         operaciones[contadorOperaciones]=op;
         opUsuarios[msg.sender].push(contadorOperaciones);
         emit nuevaOperacion(msg.sender,exportador,msg.value);
-    }
-    function validar()external{        
-        require(esValidador[msg.sender], "No eres un validador autorizado");
-
-
 
     }
-    function pagarAExportador(uint256 id)private{
+    function validar(uint256 id)external{      
+        require(msg.sender==operaciones[id].importador, "No tienes permiso para validar ");
         address exportador=operaciones[id].exportador;
+        exportadores[exportador].reputacion+=1;
+
+        address importador=operaciones[id].importador;
+        importadores[importador].reputacion += 1;
+
+        pagar(id,exportador);
+        operaciones[id].estado = Estado.exitoso;
+
+        emit OperacionExitosa(id);
+    }
+
+    function mediar(uint256 id,address ganador)public {
+        require(esIntermediario[msg.sender],"no es intermediario");
+        require(operaciones[id].estado==Estado.enDisputa,"la operacion no esta en disputa");
+        require(operaciones[id].exportador==ganador|| operaciones[id].importador==ganador,
+        "ganador no participa en operacion");
+        pagar(id,ganador);
+        operaciones[id].estado = Estado.resuelto;
+        emit DisputaResuelta(id, ganador, msg.sender);
 
 
+    }
 
-
+    function pagar(uint256 id,address venefisiario)private nonReentrant{
+        require(operaciones[id].exportador==venefisiario || operaciones[id].importador==venefisiario,
+        "veneficiario no participa en operacion");
+        uint256 montoTransacion=operaciones[id].monto;
+        uint256 comicion = (montoTransacion * porcentajeComision)/100;
+        uint256 montoAEnviar=montoTransacion-comicion;
+        ganancia+=comicion;
+        (bool sent,)=payable (venefisiario).call{value: montoAEnviar}("");
+        if (!sent){
+            revert noSePundoRealizarTransaccion();
+        }
+    }
+    function operacionesDe(address usuario) public view returns (uint[] memory) {
+        return opUsuarios[usuario];
     }
 
 }
